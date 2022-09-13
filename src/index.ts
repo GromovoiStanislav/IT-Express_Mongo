45c7d7de5fe1f057e138e798ec5b949e53c15df8
Stanislav <gromila12@rambler.ru>
2022-09-13 14:10:38 +0600

    change message

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req:Request, res:Response) => {
-    let message = 'Hello Incubator!!';
+    let message = 'Hello Incubator!!!!!';
     res.send(message)
 })
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port http://localhost:${PORT}/`)
 })
+//https://it-students-demo.herokuapp.com/
