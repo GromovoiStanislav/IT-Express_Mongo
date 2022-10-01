6a7a3cdea67ffb322bd6b780417c0182ba8daa36
Stanislav <gromila12@rambler.ru>
2022-10-01 12:11:35 +0600

    sendStatus insted send

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -4,10 +4,10 @@ import {Request, Response, NextFunction} from 'express'
 export const auth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('Authorization')
     if (!token) {
-        return res.send(401)
+        return res.sendStatus(401)
     }
     if (token !== 'Basic YWRtaW46cXdlcnR5') {
-        return res.send(401)
+        return res.sendStatus(401)
     }
     next()
 }
