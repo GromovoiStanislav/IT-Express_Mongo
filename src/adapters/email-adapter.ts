ec6dceaf1ccb347c3e603808812430493e7c4d32
Stanislav <gromila12@rambler.ru>
2022-10-17 16:59:16 +0600

    refactoring

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
 import nodemailer from 'nodemailer'
+import {settings} from '../settigs'
 
 export const emailAdapter = {
     async sendEmail(email: string, subject: string, message: string):Promise<boolean> {
@@ -7,14 +7,14 @@ export const emailAdapter = {
         const transport = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD
+                user: settings.EMAIL,
+                pass: settings.EMAIL_PASSWORD
             }
         })
 
         try {
             await transport.sendMail({
-                from: `Stas <${process.env.EMAIL}>`,
+                from: `Stas <${settings.EMAIL}>`,
                 to: email,
                 subject: subject,
                 html: message,
--- src/settigs.ts
+++ src/settigs.ts
@@ -3,4 +3,6 @@ import 'dotenv/config'
 export const settings = {
     JWT_SECRET: process.env.JWT_SECRET || 'very secret key',
     URL: process.env.URL,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    EMAIL: process.env.EMAIL,
 }
